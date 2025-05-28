5a7e598db1aad51bee14ecb9403b7f6ddf480739
dutsc <1020018707@qq.com>
2025-05-28 19:34:24 +0000

    support xpyd

--- benchmarks/disagg_benchmarks/start_pd_benchmark_serving.sh
+++ benchmarks/disagg_benchmarks/start_pd_benchmark_serving.sh
@@ -5,7 +5,7 @@ dataset_name="sharegpt"
 # dataset_name="random"
 dataset_path="/share/dataset/ShareGPT52K/sg_90k_part1.json"
 num_prompts=800
-qps=20
+qps=28
 
 # for qps in $(seq 1 1 10); do
 python3 ../benchmark_serving.py \
